d82731031df304bbafd5de0d026a4b6637330e3c
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-09-29 15:48:22 +0600

    login added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,64 @@
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import auth from "../../firebase/firebase_config";
 
 const Login = () => {
+  const [loginError, setLoginError] = useState("");
+  const [success, setSuccess] = useState("");
+  const emailRef = useRef(null);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    setLoginError("");
+    setSuccess("");
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        if (result.user.emailVerified) {
+          setSuccess("Logged in SuccessFully");
+        } else {
+          alert("Please verify your email");
+        }
+      })
+      .catch((error) => {
+        setLoginError(error.message);
+      });
+  };
+
+  const handleForgetPassword = () => {
+    const email = emailRef.currentUser.value;
+    if (!email) {
+      console.log("pelase provide an email", emailRef.current.value);
+      return;
+    } else if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
+    ) {
+      console.log("please write a valid email");
+      return;
+    }
+
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert("please check your email");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
-              <form>
+              <form onSubmit={handleLogin}>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Email</span>
@@ -16,6 +67,7 @@ const Login = () => {
                     type="text"
                     name="email"
                     placeholder="email"
+                    ref={emailRef}
                     className="input input-bordered"
                   />
                 </div>
@@ -30,7 +82,11 @@ const Login = () => {
                     className="input input-bordered"
                   />
                   <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">
+                    <a
+                      onClick={handleForgetPassword}
+                      href="#"
+                      className="label-text-alt link link-hover"
+                    >
                       Forgot password?
                     </a>
                   </label>
@@ -39,7 +95,8 @@ const Login = () => {
                   <button className="btn btn-primary">Login</button>
                 </div>
               </form>
-
+              {loginError && <p className="text-red-700">{loginError}</p>}
+              {success && <p className="text-green-600">{success}</p>}
               <p>
                 New to this website? Please <Link to="/sign-up">Register</Link>
               </p>
